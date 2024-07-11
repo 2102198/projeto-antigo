1f158762fcc76613aea04a58b84632c849721b44
2102198 <2102198@aluno.univesp.br>
2024-07-10 23:25:40 -0300

    codigo js teste

---
+++ js-teste/objeto01.js
@@ -0,0 +1,6 @@
+//Introdução a Orientação a Objetos no JS
+//é capaz de armazenar string, n°s, functions, ...
+
+let amigo = {nome:'José', sexo:'M', peso:80.5, engordar(p){}}
+
+console.log(amigo.nome)
